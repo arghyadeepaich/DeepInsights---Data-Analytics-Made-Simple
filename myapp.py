509e88da3819b2fe4cbf918060601effb688c82a
Arghyadeep Aich <140007289+arghyadeepaich@users.noreply.github.com>
2024-08-14 23:33:01 +0530

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -12,7 +12,7 @@ st.markdown(
     """
     <style>
     .stApp {
-        background-color: #000000; 
+        background-color: #5C166F; 
    
     }
     </style>
@@ -20,7 +20,7 @@ st.markdown(
     unsafe_allow_html=True,
 )
 # Application title and description
-st.title("Data Analytics Web Application")
+st.title(":white[Data Analytics Web Application"])
 st.caption("### Developed by Arghyadeep Aich")
 
 st.subheader("Data Analysis Made Simple: Upload your Excel or CSV files to effortlessly generate powerful insights—No coding skills needed!")
